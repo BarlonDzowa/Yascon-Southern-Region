7915337e948ac57c484cb34ef9e3b3ce92fcf694
BarlonDzowa <bit21-bdzowa@mubas.ac.mw>
2026-02-20 18:06:22 +0200

    WORKING PAGES

---
+++ app/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/yascon-website',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
